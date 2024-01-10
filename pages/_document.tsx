97bfa23e711a31204022964a43b13e6973015a94
HubooDeclan <declan.fitzpatrick@huboo.com>
2024-01-10 15:38:20 +0000

    Added analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import { Head, Html, Main, NextScript } from "next/document";
 import Script from "next/script";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: "GDScript Formatter",
@@ -21,13 +22,15 @@ export const metadata: Metadata = {
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9568267309357674"
-        strategy="lazyOnload"
-        crossOrigin="anonymous"
-      />
+      <Head>
+        <Analytics />
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9568267309357674"
+          strategy="lazyOnload"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
